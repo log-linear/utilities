74ab759714cc402d4749295cf7590f5047acd209
Victor Faner <vfaner@protonmail.com>
2020-05-16 22:08:29 +0000

    Update get_mullvad_ip.sh

--- get_mullvad_ip.sh
+++ get_mullvad_ip.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-# Script to pull current Mullvad-provided IPv4
-mullvad status | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}.[0-9]\{1,3\}$" > /mnt/1E3AE01D3ADFF02F/Syncthing/Sync/lore_ip.txt
+#
+# Script to pull current Mullvad-provided IPv4 off of soong
+#
+
+touch /home/victor/Sync/lore_ip.txt
+mullvad status | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}.[0-9]\{1,3\}" \
+  > /home/victor/Sync/lore_ip.txt
